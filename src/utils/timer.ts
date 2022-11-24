a7c12aa6f8360bbe1bbe24a7bd3cc1334fe3be69
meta <meta@macmini.lan>
2022-11-24 20:14:09 +0800

    fix: timer

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,5 +1,5 @@
 export const timer = () => {
-    const start = new Date('2022-08-17');
+    const start = new Date('2022-08-17 00:00:00');
     const now = Date.now();
     const diff = now - start;
     const dayInMs = 1000 * 60 * 60 * 24;
