20956cf58fd9c7ef36fdadc21e972b7ce096a0dc
bbbottle <z@zjh.im>
2022-12-04 16:14:34 +0800

    fix: data parse

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,5 +1,20 @@
+
+const parseDate = (dateString: string) => {
+    let time = Date.parse(dateString);
+    if(!time){
+        time = Date.parse(dateString.replace("T"," "));
+        if(!time){
+            const bound = dateString.indexOf('T');
+            const dateData = dateString.slice(0, bound).split('-').map(Number);
+            const timeData = dateString.slice(bound+1, -1).split(':').map(Number);
+
+            time = Date.UTC(dateData[0],dateData[1]-1,dateData[2],timeData[0],timeData[1],timeData[2]);
+        }
+    }
+    return time;
+}
 export const timer = () => {
-    const start = Date.parse('2022-08-17 00:00:00');
+    const start = parseDate('2022-08-17T00:00:00');
     const now = Date.now();
     const diff = now - start;
     const dayInMs = 1000 * 60 * 60 * 24;
